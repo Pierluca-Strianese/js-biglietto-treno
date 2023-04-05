31e9ef48e7980c021dee32255934e87167688b00
Pierluca-Strianese <pierluca.strianese@gmail.com>
2023-04-05 17:25:34 +0200

    add function for "prezzofinale"

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+
+const chilometri = prompt('Quanti km vuoi percorrere?');
+
+const eta = prompt('Quale e\' la tua eta\'');
+
+let prezzofinale = Math.round (chilometri * 0.21);
+
+if (eta < 18) {
+    prezzofinale = Math.round ((chilometri * 0.21) - ((chilometri * 0.21) / 100 * 20));
+}
+
+if (eta > 65) {
+    prezzofinale = Math.round ((chilometri * 0.21) - ((chilometri * 0.21) / 100 * 40));
+}
+
+
+console.log('km:', chilometri);
+console.log('eta:', eta);
+console.log('prezzofinale:', prezzofinale);
